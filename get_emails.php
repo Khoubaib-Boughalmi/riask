17b1143f70706948d925d6850fb043cd1599c70a
khoubaib69 <khoubaib.kb99@gmail.com>
2019-11-19 00:18:53 +0100

    fix

--- get_emails.php
+++ get_emails.php
@@ -1,8 +1,5 @@
 <?php
-    $con = mysqli_connect('localhost','root','','riask');
-    if(mysqli_connect_errno()){
-        echo 'connection failled';
-    }
+  require 'db.php';
     if (isset($_POST['suggestion'])) {
         $email_pasta =$_POST['suggestion'];
         $query_email = mysqli_query($con,"SELECT email FROM users WHERE email='$email_pasta'");
