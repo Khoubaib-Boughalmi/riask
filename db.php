7a9393b73f42b4176a56b5cf06c9a85afeba7659
khoubaib69 <khoubaib.kb99@gmail.com>
2019-11-19 00:00:30 +0100

    fix

--- db.php
+++ db.php
@@ -4,8 +4,8 @@ error_reporting(E_ALL);
 
 
 return  [
-    'host' => $_ENV['host'] or 'localhost',
-    'dbname' => $_ENV['dbname'] or 'riask',
-    'user' => $_ENV['user'] or 'root',
-    'password' => $_ENV['password'] or ''
+    'host' => isset($_ENV['host'])? $_ENV['host']: 'kf3k4aywsrp0d2is.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    'dbname' => isset($_ENV['dbname'])? $_ENV['dbname']: 'k3dqklbrwiunemb3',
+    'user' => isset($_ENV['user'])? $_ENV['user']: 'zmnwq0sa1b4ek8y6',
+    'password' => isset($_ENV['password'])? $_ENV['password']: 'g2fo2m0qwchr1wp1' 
 ];
--- header.php
+++ header.php
@@ -9,9 +9,10 @@ if (!defined('header')) {
 $timezone=  date_default_timezone_set('Africa/Tunis');
 // connection with db
 $dbData = require "db.php";
+//var_dump($dbData);
 $con = mysqli_connect($dbData['host'],$dbData['user'],$dbData['password'], $dbData['dbname']);
 if(mysqli_connect_errno()){
-    echo 'connection failled';
+    echo 'connection failled'. mysqli_connect_error();
 
 }
 
