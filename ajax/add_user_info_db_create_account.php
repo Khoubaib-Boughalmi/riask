21006e341b9f0d181312a4b0496d12531605c845
khoubaib69 <khoubaib.kb99@gmail.com>
2019-11-19 01:05:56 +0100

    fix databse on add user info bla bla

--- ajax/add_user_info_db_create_account.php
+++ ajax/add_user_info_db_create_account.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$con = mysqli_connect('localhost','root','','riask');
+require '../db.php';
 if(mysqli_connect_errno()){
      echo 'connection failled';
  
--- get_emails.php
+++ get_emails.php
@@ -1,8 +1,9 @@
 <?php
   require 'db.php';
+  
     if (isset($_POST['suggestion'])) {
-        $email_pasta =$_POST['suggestion'];
-        $query_email = mysqli_query($con,"SELECT email FROM users WHERE email='$email_pasta'");
+        $email =$_POST['suggestion'];
+        $query_email = mysqli_query($con,"SELECT email FROM users WHERE email='$email'");
         echo mysqli_num_rows($query_email);
     }
   
