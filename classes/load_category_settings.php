6c831391e7300aafa4bc3a8dfbc3dd56bf88a797
khoubaib69 <khoubaib.kb99@gmail.com>
2019-11-27 19:39:39 +0100

    change settings

--- classes/load_category_settings.php
+++ classes/load_category_settings.php
@@ -10,8 +10,8 @@ class category{
     public function load_category(){
         $user_name_logged_in = $this->user_name_logged_in;
         $query_category = mysqli_query($this->con,'SELECT * from category');
-        $query_category = mysqli_query($this->con,"SELECT * from users where user_name='$user_name_logged_in'");
-        $query_category_array = mysqli_fetch_array($query_category);
+        $query_category_user = mysqli_query($this->con,"SELECT * from users where user_name='$user_name_logged_in'");
+        $query_category_array = mysqli_fetch_array($query_category_user);
         while ($query_array=mysqli_fetch_array($query_category)) { 
             $category_list = $query_category_array['categories'];
             if (strstr($category_list,$query_array['category_val'])==false) {
