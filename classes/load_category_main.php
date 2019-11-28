44ba81908666d1904abf68d953f4f9095f30d190
khoubaib69 <khoubaib.kb99@gmail.com>
2019-11-28 21:01:41 +0100

    change main page height

--- classes/load_category_main.php
+++ classes/load_category_main.php
@@ -10,12 +10,12 @@ class category_main{
         while ($query_array=mysqli_fetch_array($query)) { 
             echo "<div class='load_friend_search_div'>
             <div class='friend_image_div_search_friend'>
-                <img src='images/category_img/math.png' alt='' srcset=''class='friend_image_search_friend'>
+                <img src='".$query_array['category_img']."' alt='' srcset=''class='friend_image_search_friend'>
             </div>
             <div class='friend_name_commen_friends'>
                 <a href='profile.php?user_profile=$'>
                 <div class='friend_name'><span>Math</span></div>
-                <div class='friend_commen_friends'><span>12 commen friend<span></div>
+                <div class='friend_commen_friends'><span>".$query_array['num_post']."<span></div>
                 </a>
             </div>
             <div class='follow_friend_button_search_friend follow_friend_button_search_friend_$'>Follow</div>
