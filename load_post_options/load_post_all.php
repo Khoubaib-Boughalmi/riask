996c9fccb2632204665e8d790d2b59bca864a2b5
khoubaib69 <khoubaib.kb99@gmail.com>
2019-11-29 19:47:41 +0100

    change create post

--- load_post_options/load_post_all.php
+++ load_post_options/load_post_all.php
@@ -175,6 +175,7 @@ if ($num_querry>0) {
         }
 		if ($comp == $query_load_post_count_array['count_db']) {
 			echo "<script>$('.all').css('opacity','0');
+							$('.all').addAttr('disabled');
 						  </script>";
 		}
 	} 
