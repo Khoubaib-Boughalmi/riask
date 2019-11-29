ab46b04fa95d71e7830b741229a258da1a8cc7ca
khoubaib69 <khoubaib.kb99@gmail.com>
2019-11-29 18:33:37 +0100

    change create post

--- load_post_options/load_post_friend.php
+++ load_post_options/load_post_friend.php
@@ -180,12 +180,12 @@ $post="";
 				
 			}
 		}
-		if ($comp == $query_load_post_count_array['count_db']-1) {
-			echo "<script>$('.friend').hide()</script>";
-			echo $comp.'<br>';
-			echo $query_load_post_count_array['count_db'];
-		}
     }
+			// if ($comp == $query_load_post_count_array['count_db']-1) {
+				echo "<script>$('.friend').hide()</script>";
+				echo $comp.'<br>';
+				echo $query_load_post_count_array['count_db'];
+			// }
 	// } 
 }   
 ?>
