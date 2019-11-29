079e71b7396cab39b317edd845c29fa208e402b9
khoubaib69 <khoubaib.kb99@gmail.com>
2019-11-29 21:28:26 +0100

    change create post

--- classes/load_category_main.php
+++ classes/load_category_main.php
@@ -16,10 +16,8 @@ class category_main{
                 <img src='".$query_array['category_img']."' alt='' srcset=''class='category_image_search_category'>
             </div>
             <div class='category_name_commen_categorys'>
-                <a href=''>
                 <div class='category_name'><span>".$query_array['category_val']."</span></div>
                 <div class='category_commen_categorys'><span>".$query_array['num_post']." Post in this category<span></div>
-                </a>
             </div>
             <div class='follow_category_button_search_category follow_category_button_search_category_$'>Follow</div>
             <div class=''></div>
