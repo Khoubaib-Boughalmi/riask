4ffc22e39e343909e66920a2c9bb8cc1ae1acbfb
khoubaib69 <khoubaib.kb99@gmail.com>
2019-12-15 19:03:16 +0100

    add manifest

---
+++ js/index.js
@@ -0,0 +1,10 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(function () {
+        console.log('Service worker registered!');
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  }
