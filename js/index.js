34d0058751488eeba521a2e61857a7158e3f37f4
khoubaib69 <khoubaib.kb99@gmail.com>
2019-12-16 10:56:31 +0100

    change sw

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
+
+if (!window.Promise) {
+  window.Promise = Promise;
+}
+
+
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker.register('/sw.js');
       console.log('service worker regitered')
     });
-  }
+}
+
+
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+})
   
--- sw.js
+++ sw.js
@@ -1,5 +1,22 @@
+var CACHE_STATIC_NAME = 'static-v1';
+
+var STATIC_FILES = [
+  '/',
+  '/index.html',
+  '/main.php',
+  '/css/front.css',
+  '/css/reaction.css',
+];
+
 self.addEventListener('install', (event) => {
     console.log('Inside the install handler:', event);
+    event.waitUntil(
+      caches.open(CACHE_STATIC_NAME)
+        .then(function (cache) {
+          console.log('[Service Worker] Precaching App Shell');
+          cache.addAll(STATIC_FILES);
+        })
+    )
   });
   
   self.addEventListener('activate', (event) => {
